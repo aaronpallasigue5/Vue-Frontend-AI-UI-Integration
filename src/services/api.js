85e396577fa97bef5e25633964964073869e871d
aaronpallasigue5 <akpogi0512@gmail.com>
2026-02-28 11:02:24 +0800

    Fix submodule error

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3000/api/moods" // points to your backend moods route
+  // Replace 'mental-health-api' with your actual Render service name later
+  baseURL: "https://mental-health-api.onrender.com/api/moods" 
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  // This is the most important line for GitHub Pages
+  base: '/Vue-Frontend-AI-UI-Integration/', 
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    }
+  }
 })
